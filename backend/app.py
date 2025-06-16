bc3bfbedf500d837b503ec65eab3dd812fc8e051
pani-03 <shubham.pani03@gmail.com>
2025-06-17 01:07:40 +0530

    adding rewquirements.txt

--- backend/app.py
+++ backend/app.py
@@ -137,4 +137,5 @@ def chat_history():
     return jsonify(history)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
